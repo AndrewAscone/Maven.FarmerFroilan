340a5842a50216dc99323e835239e2fb16b4bd58
Hanquan Liu <hanquanl0115@gmail.com>
2022-11-25 21:30:41 -0500

    kick off tests

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/ShelterTests/CropRowTests.java
@@ -0,0 +1,20 @@
+package com.zipcodewilmington.froilansfarm.ShelterTests;
+
+import org.junit.Test;
+
+public class CropRowTests {
+    @Test
+    public void cropRowConstrutorTest(){
+
+    }
+
+    @Test
+    public void cropRowIsFieldTest(){
+
+    }
+
+    @Test
+    public void cropRowStoreTest(){
+
+    }
+}
