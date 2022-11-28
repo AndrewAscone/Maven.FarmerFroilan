ba83a3c94dcaf3120d9490516e3f7d7aebfdd0f2
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-11-27 19:20:59 -0500

    Wednesday Added

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
@@ -9,20 +9,22 @@ import java.util.Random;
 public class Chicken extends Animal<Vegetable> implements Produce<EdibleEgg>, Storage {
     EdibleEgg edibleEgg;
     Vegetable vegetable;
-
     public Chicken(){
-
     }
-
     public String makeNoise() {
         return "Cluck";
     }
     public EdibleEgg yield() {
         this.edibleEgg = new EdibleEgg("egg");
-        if(!hasBeenFertilized()) {
-            return this.edibleEgg;
+        if(hasBeenFertilized()) {
+            try {
+                return null;
+
+            } catch (NullPointerException e) {
+                System.out.println("Eggs aren't edible");
+            }
         }
-        return null;
+        return this.edibleEgg;
     }
     public boolean hasBeenFertilized() {
         Random random = new Random();
@@ -33,7 +35,6 @@ public class Chicken extends Animal<Vegetable> implements Produce<EdibleEgg>, St
         else{
             return false;
         }
-
     }
     @Override
     public String getName() {
--- src/main/java/com/zipcodewilmington/froilansfarm/WeeklyActivities.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/WeeklyActivities.java
@@ -4,11 +4,6 @@ import com.zipcodewilmington.froilansfarm.Crops.*;
 
 public class WeeklyActivities {
     public static void main(String[] args) {
-        Sunday sunday = new Sunday();
-        sunday.prompt();
-        Monday monday = new Monday();
-        monday.prompt();
-        Tuesday tuesday = new Tuesday();
-        tuesday.prompt();
+        new Wednesday();
     }
 }
